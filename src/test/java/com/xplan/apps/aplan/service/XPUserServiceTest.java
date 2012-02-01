5ab9ef9779530ceea5457a4f7c87b88bfae49c71
Chivalrous <wyhailonger@qq.com>
2012-02-01 17:13:01 +0800

    del test dao service

--- src/test/java/com/xplan/apps/aplan/service/XPUserServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.xplan.apps.aplan.service;  
-
-import org.springframework.test.context.ContextConfiguration;
-import org.springside.modules.test.spring.SpringTxTestCase;
-
-@ContextConfiguration(locations = { "/applicationContext-test.xml"})
-public class XPUserServiceTest extends SpringTxTestCase {
-	
-}
