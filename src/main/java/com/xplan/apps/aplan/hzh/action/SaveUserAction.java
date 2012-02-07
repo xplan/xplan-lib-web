828deafa0f195c19450dd18acf8c45dc08e20a4b
Chivalrous <15937080@qq.com>
2012-02-07 11:30:04 +0800

    save test

--- src/main/java/com/xplan/apps/aplan/hzh/action/SaveUserAction.java
+++ src/main/java/com/xplan/apps/aplan/hzh/action/SaveUserAction.java
@@ -2,12 +2,13 @@ package com.xplan.apps.aplan.hzh.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.xplan.apps.aplan.beans.User;
-import com.xplan.apps.aplan.service.UserService;
+import com.xplan.apps.aplan.entity.XpManageUser;
+import com.xplan.apps.aplan.service.XpManageUserService;
 
 public class SaveUserAction extends ActionSupport {
 	
 	private User user;
-	private UserService userService;
+	private XpManageUserService xpManageUserService;
 
 	public User getUser() {
 		return user;
@@ -16,19 +17,15 @@ public class SaveUserAction extends ActionSupport {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	public UserService getUserService() {
-		return userService;
-	}
-
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+ 
 
 	
 	@Override
 	public String execute() throws Exception {
-		this.userService.save(this.user);  //保存用户信息到数据库
+		
+		XpManageUser x = new XpManageUser();
+		x.setXpManageUserName(user.getUsername());
+		this.xpManageUserService.saveUser(x);
 		return SUCCESS;
 	}
 
--- src/main/java/com/xplan/apps/aplan/service/XpManageUserService.java
+++ src/main/java/com/xplan/apps/aplan/service/XpManageUserService.java
@@ -17,4 +17,16 @@ public class XpManageUserService {
 	public List<XpManageUser> getAll(){
 		return xpManageUserDao.getAll();
 	}
+	/**
+	 * 
+	 * saveUser(save)   
+	 * (这里描述这个方法适用条件 – 可选)   
+	 * @param xpManageUser    
+	 *void   
+	 * @exception    
+	 * @since  1.0.0
+	 */
+	public void saveUser(XpManageUser xpManageUser){
+		xpManageUserDao.save(xpManageUser);
+	}
 }
